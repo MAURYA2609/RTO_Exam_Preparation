59b1bd55d27d7007ee03b7bc395e5a31cae28133
utsavparakhiya <utsavparakhiya6933@gmail.com>
2021-01-21 23:29:11 +0530

    Questions are seen

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { QuestionListComponent } from './question-list/question-list.component';
 
 @NgModule({
-  declarations: [AdminhomeComponent],
+  declarations: [AdminhomeComponent, QuestionListComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
